fa49d98ba1a6c825699717f15aec7444d779f406
Noha Althani <noha.althani@kcl.ac.uk>
2022-10-13 12:57:58 +0100

    Excercise Solved

--- primes.py
+++ primes.py
@@ -3,4 +3,22 @@
 
 def primes(number_of_primes):
     list = []
+
+    if (number_of_primes <= 0):
+        raise ValueError("The Value Must Be Greater Than 0.")
+
+    else:
+        num = 2
+        ctr = 0
+
+        while ctr < number_of_primes:
+            prime = True
+            for i in range (2, num):
+                if num % i == 0:
+                    prime = False
+            if prime:
+                list.append(num)
+                ctr += 1
+
+            num += 1
     return list
